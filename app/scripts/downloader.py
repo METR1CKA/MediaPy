d8acff6a373b66eea6125fa324e80bb381829c92
METR1CKA <ferchosalazar054@gmail.com>
2022-10-22 21:31:53 -0500

    [UPD] Add direct audio downloader

--- app/scripts/downloader.py
+++ app/scripts/downloader.py
@@ -2,14 +2,24 @@ from pytube import YouTube
 
 class downloader:
 
-    def __init__(self):
-        pass
+    def __init__(self, url):
 
+        self.url = url
 
-    def videoDownloader(self, url):
+    def videoDownloader(self):
 
-        yt = YouTube(url)
+        yt = YouTube(self.url)
 
         video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
 
         return video
+
+    def audioDownloader(self, name):
+
+        yt = YouTube(self.url)
+
+        audio = yt.streams.get_audio_only()
+
+        filename = audio.download(filename = name)
+
+        return filename
