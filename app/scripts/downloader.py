fea794337212edd1a08b5d02661a2eb025182d83
METR1CKA <ferchosalazar054@gmail.com>
2022-10-22 21:37:58 -0500

    [UPD] Refactor downloader class

--- app/scripts/downloader.py
+++ app/scripts/downloader.py
@@ -4,21 +4,17 @@ class downloader:
 
     def __init__(self, url):
 
-        self.url = url
+        self.yt = YouTube(url)
 
     def videoDownloader(self):
 
-        yt = YouTube(self.url)
-
-        video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+        video = self.yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
 
         return video
 
     def audioDownloader(self, name):
 
-        yt = YouTube(self.url)
-
-        audio = yt.streams.get_audio_only()
+        audio = self.yt.streams.get_audio_only()
 
         filename = audio.download(filename = name)
 
